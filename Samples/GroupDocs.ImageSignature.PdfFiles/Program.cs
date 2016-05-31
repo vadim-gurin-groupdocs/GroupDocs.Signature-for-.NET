fac842e6f95b1756f3a5695bca382aaf77440e08
vadim-gurin-groupdocs <vadim.gurin@groupdocs.com>
2016-05-31 16:28:25 +0300

    SIGNATURENET-2089 Implement Image Signature Alignment for PDF
    Updated a sample.

--- Samples/GroupDocs.ImageSignature.PdfFiles/Program.cs
+++ Samples/GroupDocs.ImageSignature.PdfFiles/Program.cs
@@ -4,6 +4,7 @@ using GroupDocs.Signature.Config;
 using GroupDocs.Signature.Handler;
 using GroupDocs.Signature.Options;
 using GroupDocs.Signature;
+using GroupDocs.Signature.Domain;
 
 namespace GroupDocs.Samples.ImageSignature.PdfFiles
 {
@@ -28,25 +29,32 @@ namespace GroupDocs.Samples.ImageSignature.PdfFiles
             SignatureHandler handler = new SignatureHandler(config);
             
             // setup PDF image signature options
-            PdfSignImageOptions options = new PdfSignImageOptions(@"Autograph_of_Benjamin_Franklin.png");
-            options.DocumentPageNumber = 1;
-            options.Top = 500;
-            //options.SignAllPages = true;
-            options.Width = 20;
-            options.Height = 10;
+            PdfSignImageOptions pdfImageSignatureOptions = new PdfSignImageOptions(@"Autograph_of_Benjamin_Franklin.png");
+            pdfImageSignatureOptions.DocumentPageNumber = 1;
+            pdfImageSignatureOptions.Top = 500;
+            pdfImageSignatureOptions.Width = 50;
+            pdfImageSignatureOptions.Height = 20;
 
             // Set a license if you have one
             License license = new License();
             license.SetLicense(@"GroupDocs.Signature3.lic");
 
+            pdfImageSignatureOptions.HorizontalAlignment = HorizontalAlignment.Left;
+            pdfImageSignatureOptions.VerticalAlignment = VerticalAlignment.Top;
+            pdfImageSignatureOptions.Margin = new Padding()
+            {
+                Right = 50,
+                Top = 50
+            };
+
             SaveOptions saveOptions = new SaveOptions(OutputType.String);
             // sign the document
             LoadOptions loadOptions = new LoadOptions();
-            string fileName = handler.Sign<string>(@"candy.pdf", options, saveOptions);
+            string fileName = handler.Sign<string>(@"candy.pdf", pdfImageSignatureOptions, saveOptions);
             Console.WriteLine("Document signed successfully. The output filename: {0}", fileName);
                 
             loadOptions.Password = "123";
-            fileName = handler.Sign<string>(@"candyPassword123.pdf", options, loadOptions, saveOptions);
+            fileName = handler.Sign<string>(@"candyPassword123.pdf", pdfImageSignatureOptions, loadOptions, saveOptions);
             Console.WriteLine("Document signed successfully. The output filename: {0}", fileName);
         }
     }
